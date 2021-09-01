4e2f5add07253c106b2e34940d805eab8c183737
Prashanth <imprash4u@gmail.com>
2021-09-01 11:38:33 +0530

    Added:Comapreto method

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -22,9 +22,10 @@ public class LineComparison {
         System.out.println("Enter y2 ");
         y2 = scanner.nextInt();
         line1 = Math.sqrt(Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2);
-        System.out.println("Length of the line: "+line1);
+        System.out.println("Length of the line1: " + line1);
         return line1;
     }
+
     public double lengthOfLine2() {
         System.out.println("Enter Cartesian Co-ordinates");
         System.out.println("Enter x3 ");
@@ -35,24 +36,26 @@ public class LineComparison {
         y3 = scanner.nextInt();
         System.out.println("Enter y4 ");
         y4 = scanner.nextInt();
-        line2= Math.sqrt(Math.pow((x4 - x3), 2)) + Math.pow((y4 - y3), 2);
-        System.out.println("Length of the line: "+line1);
+        line2 = Math.sqrt(Math.pow((x4 - x3), 2)) + Math.pow((y4 - y3), 2);
+        System.out.println("Length of the line2: " + line2);
         return line2;
     }
 
+    public void compareTo() {
+        double line1Length = lengthOfLine1();
+        double line2Length = lengthOfLine2();
+        if (line1Length == line2Length) {
+            System.out.println("Lines are Equal");
+        } else if (line1Length > line2Length) {
+            System.out.println("Line1 is grater than Line2 ");
+        } else {
+            System.out.println("lines2 is grater than line1");
+        }
+    }
+
     public static void main(String[] args) {
-        // welcome() method is not static so call by creating a object
         LineComparison lineMethods = new LineComparison();
         lineMethods.welcome();
-
-        double line1Length=lineMethods.lengthOfLine1();
-        double line2Length=lineMethods.lengthOfLine2();
-        if(line1Length == line2Length){
-            System.out.println("Lines are Equal");
-        }else
-        {
-            System.out.println("lines are not equal");
-        }
-
+        lineMethods.compareTo();
     }
 }
