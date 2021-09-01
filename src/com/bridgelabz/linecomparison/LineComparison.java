261febe76c352f04ebd3b83b11c469b72842a838
Prashanth <imprash4u@gmail.com>
2021-09-01 08:30:31 +0530

    Added: Comparing two lines

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,14 +3,15 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
-    int x1, x2, y1, y2;
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    double line1, line2;
     Scanner scanner = new Scanner(System.in);
 
     public void welcome() {
         System.out.println("......Welcome to Line Comparison Computation Program... ");
     }
 
-    public void lengthOfLine() {
+    public double lengthOfLine1() {
         System.out.println("Enter Cartesian Co-ordinates");
         System.out.println("Enter x1 ");
         x1 = scanner.nextInt();
@@ -20,15 +21,38 @@ public class LineComparison {
         y1 = scanner.nextInt();
         System.out.println("Enter y2 ");
         y2 = scanner.nextInt();
-        System.out.println("Length of the line:" + (Math.sqrt(Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2)));
-
+        line1 = Math.sqrt(Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2);
+        System.out.println("Length of the line: "+line1);
+        return line1;
+    }
+    public double lengthOfLine2() {
+        System.out.println("Enter Cartesian Co-ordinates");
+        System.out.println("Enter x3 ");
+        x3 = scanner.nextInt();
+        System.out.println("Enter x4 ");
+        x4 = scanner.nextInt();
+        System.out.println("Enter y3 ");
+        y3 = scanner.nextInt();
+        System.out.println("Enter y4 ");
+        y4 = scanner.nextInt();
+        line2= Math.sqrt(Math.pow((x4 - x3), 2)) + Math.pow((y4 - y3), 2);
+        System.out.println("Length of the line: "+line1);
+        return line2;
     }
 
     public static void main(String[] args) {
         // welcome() method is not static so call by creating a object
         LineComparison lineMethods = new LineComparison();
         lineMethods.welcome();
-        lineMethods.lengthOfLine();
+
+        double line1Length=lineMethods.lengthOfLine1();
+        double line2Length=lineMethods.lengthOfLine2();
+        if(line1Length == line2Length){
+            System.out.println("Lines are Equal");
+        }else
+        {
+            System.out.println("lines are not equal");
+        }
 
     }
 }
